dee576e92f4141dc07772189a2a3dcd4eb4920bb
sukhijaa <31399670+sukhijaa@users.noreply.github.com>
2019-11-01 02:20:52 +0530

    Registering Conversion MS with Eureka

--- currency-conversion-service/src/main/java/com/sukhijaa/udemy/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/sukhijaa/udemy/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.sukhijaa.udemy.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.sukhijaa.udemy.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
